4b667fecb2fdfa15569cdb7265029e9502f3f00e
ArtyomKaltovich <KaltovichArtyom@gmail.com>
2022-09-06 23:02:28 +0300

    add performance test in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::time::Instant;
+use algorithms::{fib, fib_simple};
+
 fn main() {
-    println!("Hello, world!");
+    let t1 = Instant::now();
+    for _ in 0..100 { // use cycle, because on 100 elements its overflows
+        fib_simple(90);
+    }
+    let t2 = Instant::now();
+    for _ in 0..100 {
+        fib(90);
+    }
+    let t3 = t2.elapsed();
+    println!("Elapsed on fib simple: {:.2?}", t2 - t1);
+    println!("Elapsed on fib: {:.2?}", t3);
+    // in debug fib is > 10x faster in release - slower :)
 }
