bd59e97b5334aa9f3408cb6d4d4b58b7eb30240a
Artsiom Kaltovich <a_kaltovich@wargaming.net>
2023-04-04 20:45:30 +0300

    switch from bin to examples

---
+++ examples/bench_fib.rs
@@ -0,0 +1,16 @@
+use std::time::Instant;
+use algorithms::{fib, fib_simple};
+
+fn main() {
+    run_fib();
+}
+
+fn run_fib() {
+    let t1 = Instant::now();
+    let r = fib_simple::<i128>(180);
+    let t2 = Instant::now();
+    let r2 = fib::<i128>(180);
+    let t3 = t2.elapsed();
+    println!("fib simple = {}, time elapsed: {:.2?}", r, t2 - t1);
+    println!("fib = {}, time elapsed: {:.2?}", r2, t3);
+}
--- examples/bench_sort.rs
+++ examples/bench_sort.rs
@@ -1,5 +1,4 @@
 use std::time::Instant;
-use algorithms::{fib, fib_simple};
 use algorithms::sort::{bubble_sort, insert_sort};
 use rand::prelude::*;
 
@@ -23,14 +22,4 @@ fn run_sort() {
     let e_bubble = t.elapsed();
     println!("insert sort simple time elapsed: {:.2?}", e_insert);
     println!("bubble sort simple time elapsed: {:.2?}", e_bubble);
-}
-
-fn run_fib() {
-    let t1 = Instant::now();
-    let r = fib_simple::<i128>(180);
-    let t2 = Instant::now();
-    let r2 = fib::<i128>(180);
-    let t3 = t2.elapsed();
-    println!("fib simple = {}, time elapsed: {:.2?}", r, t2 - t1);
-    println!("fib = {}, time elapsed: {:.2?}", r2, t3);
 }
